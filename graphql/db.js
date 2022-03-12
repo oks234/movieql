90cd5566eed1667f4eaf412db6504a0be1ca7c19
dean-oks <oks234@deancorporation.com>
2022-03-12 22:34:37 +0900

    #0.13 Wrapping a REST API with GraphQL Part Two

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,17 @@
-import fetch from "node-fetch";
+import fetch from "cross-fetch";
 
-const API_URL = "https://yts.mx/api/v2/list_movies.json";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-const getMovies = (limit, rating) => {
-  fetch(`${API_URL}?limit=50&minimum_rating=9`)
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+
+  return fetch(REQUEST_URL)
     .then((res) => res.json())
     .then((json) => json.data.movies);
-};
-
-export const getById = (id) => movies.find((movie) => movie.id === id);
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: getMovies().length,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,8 @@
-import { addMovie, deleteMovie, getById, getMovies } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
   },
 };
 
